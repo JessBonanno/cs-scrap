a9a721990019c5977fc1a26bfe5988b49f577245
Jess <jessbonanno@gmail.com>
2020-11-17 20:38:13 -0500

    add two huge numbers linked list problem

--- codesignal.py
+++ codesignal.py
@@ -578,45 +578,91 @@ b = [9999, 9999, 9999, 9999, 9999, 9999]
 
 
 
+# my code couldn't get to pass
+# def addTwoHugeNumbers(a, b):
+#     if a is None and b is None:
+#         return [0]
+#     if a.value == 0:
+#         return b
+#
+#     cur_a = a
+#     cur_b = b
+#     a_val = ''
+#     b_val = ''
+#     a_count = len(str(a.value))
+#     b_count = len(str(b.value))
+#     count = max(a_count, b_count)
+#     print('count:', count)
+#     while cur_a is not None:
+#         zeros = count - len(str(cur_a.value))
+#         a_val += '0'*zeros
+#         a_val += str(cur_a.value)
+#         cur_a = cur_a.next
+#
+#     while cur_b is not None:
+#         b_val += str(cur_b.value)
+#         cur_b = cur_b.next
+#
+#     result = str(int(a_val) + int(b_val))
+#     print('a:', a_val)
+#     print('b:', b_val)
+#     print(result)
+#     n = count
+#
+#     res_arr = [int(result[i:i+n]) for i in range(0, len(result), n)]
+#     print('res arr', res_arr)
+#     for num in res_arr:
+#         if num > 0:
+#             num = int(str(num).rstrip('0'))
+#         if len(str(a[0])) == 1:
+#             num = int(str(num).rstrip('0'))
+#
+#     return res_arr
+#
+# print(addTwoHugeNumbers(a, b))
+
+
+# Definition for singly-linked list:
+# class ListNode(object):
+#   def __init__(self, x):
+#     self.value = x
+#     self.next = None
+#
+
+# top codesignal passing solution
 def addTwoHugeNumbers(a, b):
-    if a is None and b is None:
-        return [0]
-    if a.value == 0:
-        return b
+    a = reverse(a)
+    b = reverse(b)
 
-    cur_a = a
-    cur_b = b
-    a_val = ''
-    b_val = ''
-    a_count = len(str(a.value))
-    b_count = len(str(b.value))
-    count = max(a_count, b_count)
-    print('count:', count)
-    while cur_a is not None:
-        zeros = count - len(str(cur_a.value))
-        a_val += '0'*zeros
-        a_val += str(cur_a.value)
-        cur_a = cur_a.next
+    carry = 0
+    result = None
 
-    while cur_b is not None:
-        b_val += str(cur_b.value)
-        cur_b = cur_b.next
+    while a is not None or b is not None or carry > 0:
+        raw = ((a.value if a is not None else 0) +
+               (b.value if b is not None else 0) +
+               carry)
 
-    result = str(int(a_val) + int(b_val))
-    print('a:', a_val)
-    print('b:', b_val)
-    print(result)
-    n = count
+        node = ListNode(raw % 10000)
+        node.next = result
 
-    res_arr = [int(result[i:i+n]) for i in range(0, len(result), n)]
-    print('res arr', res_arr)
-    for num in res_arr:
-        if num > 0:
-            num = int(str(num).rstrip('0'))
-        if len(str(a[0])) == 1:
-            num = int(str(num).rstrip('0'))
+        result = node
+        carry = raw // 10000
 
-    return res_arr
+        if a is not None:
+            a = a.next
+        if b is not None:
+            b = b.next
 
-print(addTwoHugeNumbers(a, b))
+    return result
+
+
+def reverse(list):
+    current = list
+    previous = None
+
+    while current is not None:
+        previous, current.next, current = current, previous, current.next
+
+    return previous
+
 
